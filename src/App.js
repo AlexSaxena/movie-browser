3e21e1a8c5874a65c732bbea42d01d7f58711469
AlexSaxena <saxenaalex@gmail.com>
2023-01-02 15:14:43 +0100

    Adds Movie API request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
+import { useEffect } from "react";
+
+// Movie: Key -> b24517dd
+
+const API_URL = "http://www.omdbapi.com/?apikey=b24517dd&";
 
 function App() {
+  const movieQuery = async (title) => {
+    const response = await fetch(`${API_URL}s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    movieQuery("Spiderman");
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
