49d653ea6e67ec4353933eba75b429f38bab980f
AlexSaxena <saxenaalex@gmail.com>
2023-01-04 13:42:22 +0100

    Adds Navbar structure & Style & Link to AddItems

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Browser from "./pages/Browser";
-import Favourites from "./pages/Favourites";
+import Watchlist from "./pages/WatchList";
+import AddListItem from "./pages/AddListItem";
+import "./css/NavbarStyle.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <header className="header-section">
-        <Link to="/">Browser</Link>
-        <Link to="/favourites">Favorites</Link>
+      <header className="nav-bar">
+        <h2>Movie Browser</h2>
+        <ul>
+          <li>
+            <Link to="/">Browser</Link>
+          </li>
+          <li>
+            <Link to="/watchlist">Watchlist</Link>
+          </li>
+          <li>
+            <Link to="/addListItem">AddListItem</Link>
+          </li>
+        </ul>
       </header>
       <Routes>
         <Route path="/" element={<Browser />} />
-        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/addListItem" element={<AddListItem />} />
       </Routes>
     </BrowserRouter>
   );
